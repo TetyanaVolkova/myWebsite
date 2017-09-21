e9dc02f7d0d73d5c9abca53b6ab8bec78b9b8c9a
Tetyana <tatankavolkova@gmail.com>
2017-09-20 21:26:42 -0400

    Codepen example removed for test.

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,8 +3,7 @@ var React = require('react');
 var Examples = (props) => {
 	return (
 		<div>
-			<p data-height="265" data-theme-id="0" data-slug-hash="jwdvxa" data-default-tab="css,result" data-user="tetnuc" data-embed-version="2" data-pen-title="My Info on 3D cube" class="codepen">See the Pen <a href="https://codepen.io/tetnuc/pen/jwdvxa/">My Info on 3D cube</a> by Tetyana Volkova (<a href="https://codepen.io/tetnuc">@tetnuc</a>) on <a href="https://codepen.io">CodePen</a>.</p>
-			<script async src="https://production-assets.codepen.io/assets/embed/ei.js"></script>
+		Heey!!!!
 		</div>
 	)
 }
